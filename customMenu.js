b0dab64bdb1839e839f1059fc753e94e720d10bc
Midra <midra.sub@gmail.com>
2021-02-21 02:55:17 +0900

    とりあえず完成

--- customMenu.js
+++ customMenu.js
@@ -23,32 +23,31 @@
     'block': 'block',
     'report-tweet': 'report'
   };
-
-  // カスタムメニュー用CSS
+  //////////////////////////////////////////////////////////////////////////////
+  /* カスタムメニュー用CSS */
   if(!document.getElementById('md-style-menu')){
     const style = document.createElement('style');
     style.id = 'md-style-menu';
     style.type = 'text/css';
-    style.innerHTML = '.dropdown-menu .caret{display:none}#md-custom-menu{display:block;position:fixed;top:0;left:0;z-index:999;width:100vw;height:100vh;background-color:#0000;transition-property:background-color;transition-duration:.13s}#md-custom-menu.md-cmenu-open{background-color:#00000060}#md-custom-menu.md-cmenu-hide{display:none}#md-custom-menu>.dropdown-menu{display:block!important;position:absolute!important;top:unset!important;bottom:0!important;left:0!important;right:unset!important;margin:0!important;width:100%;border-radius:15px 15px 0 0!important;box-shadow:0 0 7px 0 rgb(0 0 0 / 13%)!important;font-size:14px!important}#md-custom-menu>.dropdown-menu li.is-selectable{position:relative;height:35px}#md-custom-menu>.dropdown-menu li>.md-cmenu-icon,#md-custom-menu>.dropdown-menu li>svg{display:inline-block;position:absolute;top:0;left:20px;width:auto;height:21px;line-height:21px;padding:7px 0;box-sizing:content-box!important;fill:currentcolor}#md-custom-menu>.dropdown-menu li>a{display:inline-block;width:100%;height:100%;line-height:35px;padding:0 25px 0 50px}#md-custom-menu>.dropdown-menu li.is-selectable>a:after{font-size:14px!important}#md-custom-menu .icon:before{font-weight:400}.md-icon-mention:before{content:"\\F064"}.md-icon-copylink:before{content:"\\F098"}.md-icon-message:before{content:"\\F054"}.md-icon-follow:before{content:"\\F175"}.md-icon-favorite:before{content:"\\F148"}.md-icon-addList:before{content:"\\F712"}.md-icon-addCollection{transform:scale(-1,1)}.md-icon-addCollection:before{content:"\\F214"}.md-icon-search:before{content:"\\F058"}.md-icon-mute:before{content:"\\F101"}.md-icon-block:before{content:"\\E609"}.md-icon-report:before{content:"\\F038"}.md-icon-remove:before{content:"\\F154"}';
+    style.innerHTML = '.dropdown-menu .caret{display:none}#md-custom-menu{display:block;position:fixed;top:0;left:0;z-index:999;width:100vw;height:100vh;background-color:#0000;will-change:background-color;transition-property:background-color;transition-duration:.13s}#md-custom-menu.md-cmenu-open{background-color:#00000060}#md-custom-menu.md-cmenu-hide{height:0}#md-custom-menu>.dropdown-menu{display:block!important;position:absolute!important;top:unset!important;bottom:0!important;left:0!important;right:unset!important;margin:0!important;width:100%;border-radius:15px 15px 0 0!important;box-shadow:0 0 7px 0 rgb(0 0 0 / 13%)!important;font-size:14px!important;will-change:transform;transition:.13s cubic-bezier(.4,0,.2,1)}.md-drag{transition:0s!important}#md-custom-menu>.dropdown-menu>.md-menu-handle{display:block;width:30px;height:4px;border-radius:2px;margin:0 calc((100vw - 30px)/ 2) 5px;background-color:currentcolor}#md-custom-menu>.dropdown-menu li.is-selectable{position:relative;height:35px}#md-custom-menu>.dropdown-menu li>.md-cmenu-icon,#md-custom-menu>.dropdown-menu li>svg{display:inline-block;position:absolute;top:0;left:20px;width:auto;height:21px;line-height:21px;padding:7px 0;box-sizing:content-box!important;fill:currentcolor}#md-custom-menu>.dropdown-menu li>a{display:inline-block;width:100%;height:100%;line-height:35px;padding:0 25px 0 50px}#md-custom-menu>.dropdown-menu li.is-selectable>a:after{font-size:14px!important}#md-custom-menu .icon:before{font-weight:400}.md-icon-mention:before{content:"\\F064"}.md-icon-copylink:before{content:"\\F098"}.md-icon-message:before{content:"\\F054"}.md-icon-follow:before{content:"\\F175"}.md-icon-favorite:before{content:"\\F148"}.md-icon-addList:before{content:"\\F712"}.md-icon-addCollection{transform:scale(-1,1)}.md-icon-addCollection:before{content:"\\F214"}.md-icon-search:before{content:"\\F058"}.md-icon-mute:before{content:"\\F101"}.md-icon-block:before{content:"\\E609"}.md-icon-report:before{content:"\\F038"}.md-icon-remove:before{content:"\\F154"}';
     document.head.appendChild(style);
   }
-  // カスタムメニュー用要素
+  /* カスタムメニュー用要素 */
   let cmenuElem;
   if(!document.getElementById('md-custom-menu')){
     const elem = document.createElement('div');
     elem.id = 'md-custom-menu';
     elem.classList.add('md-cmenu-hide');
-    elem.addEventListener('click', (e)=>{
-      closeCustomMenu();
-    });
+    // elem.addEventListener('click', closeCustomMenu);
+    elem.addEventListener('touchstart', fncTouchStart, {passive: true});
+    elem.addEventListener('touchmove', fncTouchMove, {passive: true});
+    elem.addEventListener('touchend', fncTouchEnd, {passive: true});
     cmenuElem = document.body.appendChild(elem);
   }
-
-  // カスタムメニューを開く
+  //////////////////////////////////////////////////////////////////////////////
+  /* カスタムメニューを開く */
   function openCustomMenu(menu){
     if(cmenuElem){
-      // 元の要素を移動
-      cmenuElem.appendChild(menu);
       // クラス追加・削除
       cmenuElem.classList.remove('md-cmenu-hide');
       cmenuElem.classList.add('md-cmenu-open');
@@ -65,27 +64,142 @@
           }
         }
       }
+      // ハンドル追加
+      if(!menu.querySelector('div.md-menu-handle')){
+        menu.insertAdjacentHTML('afterbegin', '<div class="md-menu-handle"></div>');
+      }
+      // style
+      menu.style.transform = `translate3d(0px, ${menu.clientHeight}px, 0px)`;
+      // 元の要素を移動
+      cmenuElem.appendChild(menu);
+      // 上に移動
+      setTimeout(()=>{
+        menu.style.transform = 'translate3d(0px, 0px, 0px)';
+      }, 0);
     }
   }
-
-  // カスタムメニューを閉じる
+  /* カスタムメニューを閉じる */
   function closeCustomMenu(){
     if(!cmenuElem) return;
-    // 要素内を削除
-    while(cmenuElem.firstChild){
-      cmenuElem.removeChild(cmenuElem.firstChild);
+    const menu = cmenuElem.querySelector('div.dropdown-menu');
+    if(menu){
+      // アニメーション用に要素を複製
+      const cloneMenu = menu.cloneNode(true);
+      cmenuElem.appendChild(cloneMenu);
+      const mh = menu.clientHeight;
+      // 元のメニューを削除
+      menu.remove();
+      // 下に移動
+      setTimeout(()=>{
+        cloneMenu.style.transform = `translate3d(0px, ${mh}px, 0px)`;
+      }, 0);
     }
-    // クラス追加・削除
+
+    // 背景を透明にする
     cmenuElem.classList.remove('md-cmenu-open');
+
     setTimeout(()=>{
+      // 非表示
       cmenuElem.classList.add('md-cmenu-hide');
-    }, 130);
+      // 要素内を削除
+      while(cmenuElem.firstChild){
+        cmenuElem.removeChild(cmenuElem.firstChild);
+      }
+    }, 140);
   }
+  //////////////////////////////////////////////////////////////////////////////
+  /* 2点間の距離を計算（2乗した状態で返す） */
+  function getDistance(start, move){
+    const diffX = Math.abs(move.x - start.x);
+    const diffY = Math.abs(move.y - start.y);
+    return ((diffX * diffX) + (diffY * diffY));
+  }
+  //////////////////////////////////////////////////////////////////////////////
+  let touch_target;
+  let touch_ddm;
+  let touch_action;
+  let startPoint;
+  let movePoint;
+  let flgClick = true;
+  let flgClose = false;
+  //////////////////////////////////////////////////////////////////////////////
+  function fncTouchStart(e){
+    // e.preventDefault();
+    // 要素
+    touch_target = e.target;
+    touch_ddm = touch_target.closest('div.dropdown-menu');
+    touch_action = touch_target.closest('a[data-action]');
 
-  // document.body.addEventListener('click', (e)=>{
-  //
-  // });
+    // 座標
+    const touches = e.touches;
+    if(!startPoint){
+      startPoint = {
+        x: touches[0].pageX,
+        y: touches[0].pageY
+      };
+    }
+    if(!movePoint) movePoint = startPoint;
+  }
+  //////////////////////////////////////////////////////////////////////////////
+  function fncTouchMove(e){
+    // e.preventDefault();
+    // ドロップダウンメニュー外をタッチしていた場合
+    if(!touch_ddm) return;
+    // 座標
+    const touches = e.touches;
+    movePoint = {
+      x: touches[0].pageX,
+      y: touches[0].pageY
+    };
 
+    // クリックしないと判断されている場合
+    if(!flgClick){
+      // ドラッグ中
+      touch_ddm.classList.add('md-drag');
+      // セットするX軸の値
+      let translateY = (movePoint.y - startPoint.y) - 5;
+      // はみ出さないようにする
+      if(translateY < 0) translateY = 0;
+      // 一定距離を超えたら閉じるようにする
+      if(translateY > 130){
+        flgClose = true;
+      }else{
+        flgClose = false;
+      }
+      // 値をセット
+      touch_ddm.style.transform = `translate3d(0px, ${translateY}px, 0px)`;
+      return;
+    }
+    // 距離からクリックするかどうか判断
+    if(flgClick && 20 < getDistance(startPoint, movePoint)){
+      flgClick = false;
+    }
+  }
+  //////////////////////////////////////////////////////////////////////////////
+  function fncTouchEnd(e){
+    touch_target = null;
+    startPoint = null;
+    movePoint = null;
+    flgClick = true;
+    // e.preventDefault();
+    // ドロップダウンメニュー外をタッチしていたら閉じる
+    if(!touch_ddm){
+      closeCustomMenu();
+      return;
+    }
+
+    touch_ddm.classList.remove('md-drag');
+    if(flgClose){
+      touch_ddm.style.transform = `translate3d(0px, ${touch_ddm.innerHeight}px, 0px)`;
+      closeCustomMenu();
+    }else{
+      touch_ddm.style.transform = 'translate3d(0px, 0px, 0px)';
+    }
+
+    flgClose = false;
+  }
+  //////////////////////////////////////////////////////////////////////////////
+  /* 監視 */
   let observer;
   let target;
   let config;
@@ -97,7 +211,7 @@
         const addedNodes = e[i].addedNodes;
         const added = addedNodes ? addedNodes[0] : null;
         if(added && added.classList && added.classList.contains('dropdown-menu')){
-          if(added.closest('a[data-action="message-menu"]') == null && added.closest('nav.app-navigator') == null){
+          if(!added.closest('a[data-action="message-menu"]') && !added.closest('nav.app-navigator') && !added.closest('#md-custom-menu')){
             openCustomMenu(added);
           }
         }else{
